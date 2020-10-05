cc0dac961d8e85e07821744b2f7b60455a2ca15b
4l3-j4ndr0 <al3jandro9400@gmail.com>
2020-10-05 09:51:44 -0400

    initial commit

---
+++ src/mac_offline/Mac_Offline.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mac_offline;
+
+/**
+ *
+ * @author 4l3
+ */
+import java.io.IOException;
+
+public class Mac_Offline{
+
+   public static void main(String[] args) throws IOException{
+
+      visual v=new visual();
+      v.show();
+    }
+}
